c6d82a07dedc0eaf553dd074589059806237731e
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-13 22:02:29 +0000

    revert main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css"
 import * as bootstrap from 'bootstrap';
@@ -9,8 +8,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )

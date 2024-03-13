1c3695813f7c7975b38a423a827c2b58ae17ce6c
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-13 22:06:14 +0000

    delete axios

--- src/components/StockSearch.jsx
+++ src/components/StockSearch.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from "axios";
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale, Filler } from 'chart.js';
 import 'chartjs-adapter-date-fns';

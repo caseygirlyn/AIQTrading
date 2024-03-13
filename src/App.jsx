a26f1e5843e35a05151c1d141de65ebd3009e5bc
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-13 22:02:06 +0000

    revert App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Container from './components/Container';
-import SignUp from './components/pages/SignUp';
 
 const App = () => {
-  return <>
-      <Routes>
-        <Route path="/" element={<Container />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-      </Routes>
-    </>
+  return <Container />;
 }
 
 export default App;

beb3bf626c757fa0605622fdbfb589e9f08c4868
rupweb <rupert@webstersystems.co.uk>
2024-03-14 19:40:44 +0000

    Added Alpaca orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Container from './components/Container';
+import Portfolio from './components/Portfolio';
 
 const App = () => {
-  return <Container />;
+  return (
+    <Router>
+      <Routes>
+        Start App
+        <Route path="/" element={<Container />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import AlpacaOrder from './alpaca/Order'
+
+const Portfolio = () => {
+  const [selectedTicker, setSelectedTicker] = useState(null);
+
+  const tickers = [
+    { symbol: 'NVDA', name: 'NVIDIA Corporation' },
+    { symbol: 'META', name: 'Meta Platforms, Inc.' },
+    { symbol: 'NFLX', name: 'Netflix, Inc.' },
+    { symbol: 'AVGO', name: 'Broadcom Inc.' },
+    { symbol: 'QCOM', name: 'QUALCOMM Incorporated' },
+    { symbol: 'AAPL', name: 'Apple Inc.' },
+    { symbol: 'MSFT', name: 'Microsoft Corporation' },
+    { symbol: 'AMZN', name: 'Amazon.com Inc.' },
+    { symbol: 'GOOGL', name: 'Alphabet Inc. (Google)' },
+    { symbol: 'FB', name: 'Facebook, Inc.' }
+  ];
+
+  const handleOrder = async (symbol, side) => {
+    console.log('In handleOrder');
+    try {
+      const response = await placeOrder(symbol, amount, side);
+      setOrderResponse(response);
+    } catch (error) {
+      setOrderResponse(`Error: ${error.message}`);
+    }
+  };
+
+  return (
+    <div>
+      Available stocks
+      {tickers.map(ticker => (
+        <div key={ticker.symbol}>
+          {ticker.symbol} - {ticker.name}
+          <button onClick={() => setSelectedTicker(ticker)}>Trade</button>
+        </div>
+      ))}
+
+      {selectedTicker && (
+        <AlpacaOrder symbol={selectedTicker.symbol} />
+      )}
+    </div>
+  );
+};
+
+export default Portfolio;

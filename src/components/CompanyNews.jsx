477067aa8e09f8844701ac387cfb1ce9ec658c94
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-17 10:57:07 +0000

    refactor CompanyNews component

--- src/components/CompanyNews.jsx
+++ src/components/CompanyNews.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 const CompanyNews = (props) => {
     const query = props.parseQuery;
@@ -7,17 +6,22 @@ const CompanyNews = (props) => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const apiKeyNews = import.meta.env.VITE_API_KEY_NEWS_2; // Netlify ENV variable
+    const apiKeyNews = import.meta.env.VITE_API_KEY_NEWS; // Netlify ENV variable
 
     useEffect(() => {
         const fetchNews = async () => {
             try {
-                const response = await axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKeyNews}`);
-                setNews(response.data.articles);
+                const response = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKeyNews}`); // PROD
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                const data = await response.json();
+                setNews(data.articles);
             } catch (error) {
                 try {
-                    const fallbackResponse = await axios.get('/newsApi.json');
-                    setNews(fallbackResponse.data.articles);
+                    const fallbackResponse = await fetch(`/newsApi.json`); // Fallback data in case maximum request has been reached :)
+                    const fallbackData = await fallbackResponse.json();
+                    setNews(fallbackData.articles);
                 } catch (error) {
                     setError('An error occurred while fetching data');
                 }
@@ -25,12 +29,11 @@ const CompanyNews = (props) => {
                 setLoading(false);
             }
         };
-
         fetchNews();
     }, []);
 
     return (
-        <div className='container news p-2'>
+        <div className='container news'>
             <h2 className="fs-4 mb-0">{query} News</h2>
             {loading ? (
                 <div className="spinner-border text-info mx-auto my-5 d-block" role="status">
@@ -41,14 +44,8 @@ const CompanyNews = (props) => {
             ) : (
                 <div className='wrapper card-group pb-3'>
                     {news.slice(0, 12).map((article, index) => (
-                        <div className="cardContainer col-md-4 col-sm-12 w-lg-auto py-3" key={index}>
-                            <div className="card border-2 rounded-0 w-auto border-0">
-                                <div className="card-body caption py-1 px-md-2 px-0">
-                                    <p className="card-title mb-0">
-                                        <a href={article.url} className="text-decoration-none" target="_blank" rel="noopener noreferrer">{article.title}</a>
-                                    </p>
-                                </div>
-                            </div>
+                        <div className="cardContainer caption col-md-4 col-sm-12 w-lg-auto py-3" key={index}>
+                            <a href={article.url} className="text-decoration-none newsTitle" target="_blank" rel="noopener noreferrer">{article.title}</a>
                         </div>
                     ))}
                 </div>
@@ -57,4 +54,4 @@ const CompanyNews = (props) => {
     );
 };
 
-export default CompanyNews
+export default CompanyNews;

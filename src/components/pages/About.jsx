acbe371632e31457d2905b867fee1dfec9c0ad7b
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-18 17:55:38 +0000

    add about us placeholder

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+
+function About() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About

492177a84fe2c858fdb1a4c66332d595466a1a11
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-18 18:47:27 +0000

    update style

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import AlpacaOrder from './alpaca/Order'
+import Col from "./common/Theme/Col";
+import Row from "./common/Theme/Row";
 import Header from './common/Header';
 import Footer from './common/Footer';
 
 const Portfolio = () => {
   const [isDarkMode, setIsDarkMode] = useState(getInitialMode(true));
   function getInitialMode() {
-      const savedMode = JSON.parse(localStorage.getItem('darkMode'));
-      return savedMode || false; // If no saved mode, default to light mode
+    const savedMode = JSON.parse(localStorage.getItem('darkMode'));
+    return savedMode || false; // If no saved mode, default to light mode
   }
 
   // Function to toggle between dark and light mode
   const toggleDarkMode = () => {
-      setIsDarkMode(prevMode => !prevMode);
+    setIsDarkMode(prevMode => !prevMode);
   };
 
   const getTickerStyle = (index) => ({
-    backgroundColor: isDarkMode 
-      ? (index % 2 === 0 ? ' #3B404E' : '#303441') 
+    backgroundColor: isDarkMode
+      ? (index % 2 === 0 ? ' #3B404E' : '#303441')
       : (index % 2 === 0 ? '#f0f0f0' : '#fff'),
     color: isDarkMode ? 'white' : '#3d4354',
     padding: '8px',
@@ -30,14 +32,11 @@ const Portfolio = () => {
 
   const getTickerContainerStyle = () => ({
     flex: '1',
-    marginRight: '20px',
-    maxWidth: '50%'
   });
-  
+
   const getMainDivStyle = () => ({
     display: 'flex',
     backgroundColor: isDarkMode ? '#292D3A' : '#fff',
-    padding: '20px',
     borderRadius: '8px'
   });
 
@@ -61,7 +60,7 @@ const Portfolio = () => {
     borderRadius: '4px',
     cursor: 'pointer'
   });
-  
+
 
   const [selectedTicker, setSelectedTicker] = useState(null);
 
@@ -72,18 +71,18 @@ const Portfolio = () => {
     }
     setSelectedTicker(ticker);
   };
-  
+
   const tickers = [
+    { symbol: 'MSFT', name: 'Microsoft Corporation' },
+    { symbol: 'AAPL', name: 'Apple Inc.' },
     { symbol: 'NVDA', name: 'NVIDIA Corporation' },
+    { symbol: 'GOOGL', name: 'Alphabet Inc. (Google)' },
+    { symbol: 'AMZN', name: 'Amazon.com Inc.' },
     { symbol: 'META', name: 'Meta Platforms, Inc.' },
+    { symbol: 'TSLA', name: 'Tesla, Inc.' },
     { symbol: 'NFLX', name: 'Netflix, Inc.' },
     { symbol: 'AVGO', name: 'Broadcom Inc.' },
-    { symbol: 'QCOM', name: 'QUALCOMM Incorporated' },
-    { symbol: 'AAPL', name: 'Apple Inc.' },
-    { symbol: 'MSFT', name: 'Microsoft Corporation' },
-    { symbol: 'AMZN', name: 'Amazon.com Inc.' },
-    { symbol: 'GOOGL', name: 'Alphabet Inc. (Google)' },
-    { symbol: 'FB', name: 'Facebook, Inc.' }
+    { symbol: 'QCOM', name: 'QUALCOMM Incorporated' }
   ];
 
   return (
@@ -100,29 +99,39 @@ const Portfolio = () => {
         </label>
       </div>
       <Header />
-      <div style={getMainDivStyle()}>
-        <div style={getTickerContainerStyle()}>
-          <h3 style={{ color: isDarkMode ? 'white' : '#3d4354' }}>Available stocks</h3> {/* Adjust color for light mode */}
-          {tickers.map((ticker, index) => (
-            <div key={ticker.symbol} style={getTickerStyle(index)}>
-              <span style={{ marginRight: '10px' }}>{ticker.symbol} - {ticker.name}</span>
-              <div>
-                <button style={getBuyButtonStyle(index)} onClick={() => handleTickerSelection(ticker)}>Buy</button>
-                <button style={getSellButtonStyle(index)} onClick={() => handleTickerSelection(ticker)}>Sell</button>
-              </div>
+
+      <div className="container m-auto d-md-flex">
+        <Col size="md-6">
+          <div style={getMainDivStyle()} className='container mt-5 py-5'>
+            <div style={getTickerContainerStyle()} className='col-md-2'>
+              <h3 style={{ color: isDarkMode ? 'white' : '#3d4354' }}>Available stocks</h3> {/* Adjust color for light mode */}
+              {tickers.map((ticker, index) => (
+                <div key={ticker.symbol} style={getTickerStyle(index)}>
+                  <span style={{ marginRight: '10px' }}>{ticker.symbol} - {ticker.name}</span>
+                  <div style={{ minWidth: '135px' }}>
+                    <button style={getBuyButtonStyle(index)} onClick={() => handleTickerSelection(ticker)}>Buy</button>
+                    <button style={getSellButtonStyle(index)} onClick={() => handleTickerSelection(ticker)}>Sell</button>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        </Col>
+        <Col size="md-6">
+          <div style={{ flex: '1' }} className='container mt-md-5 py-md-5'>
+            {selectedTicker && (
+              <div style={{ padding: '20px', marginTop: '50px', borderRadius: '8px' }}>
+                <AlpacaOrder symbol={selectedTicker.symbol} isDarkMode={isDarkMode} />
+              </div>
+            )}
+          </div>
+        </Col>
+
       </div>
 
-      <div style={{ flex: '1' }}>
-        {selectedTicker && (
-          <div style={{ padding: '20px', marginTop: '100px', borderRadius: '8px' }}>
-            <AlpacaOrder symbol={selectedTicker.symbol} isDarkMode={isDarkMode} />
-          </div>
-        )}
-      </div>
-    </div><Footer />
+
+      <Footer />
+    </div>
     </>
   );
 };

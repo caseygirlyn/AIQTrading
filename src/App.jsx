abd53963cf63c9e0846ddb940ddba81c1c40f678
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-03-19 20:20:43 +0000

    update main login with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Container from './components/Container';
 import SignUp from './components/pages/SignUp';
 import Portfolio from './components/Portfolio';
 import Contact from './components/pages/Contact';
+import Login from './components/pages/Login';
 
 const App = () => {
   return <>
@@ -12,6 +13,8 @@ const App = () => {
       <Route path="signup" element={<SignUp />}></Route>
       <Route path="portfolio" element={<Portfolio />} />
       <Route path="contact" element={<Contact />} />
+      <Route path="login" element={<Login />} />
+
     </Routes>
   </>
 }

e59145dc0632539feabe3b15c1066444e51fc2cc
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-20 17:54:08 +0000

    update portfolio style

--- src/components/alpaca/PortfolioStatus.jsx
+++ src/components/alpaca/PortfolioStatus.jsx
@@ -73,7 +73,7 @@ const PortfolioStatus = () => {
                         <span className="p-1 d-block fs-6">RETURN</span>
                         <span className={`p-1 fs-3 ${calculatePercentageChange(portfolio.last_equity, portfolio.equity) > 0 ? 'text-success' : 'text-danger'}`}>
                             {calculatePercentageChange(portfolio.last_equity, portfolio.equity)}%
-                            {calculatePercentageChange(portfolio.last_equity, portfolio.equity) > 0 ? <i className="bi bi-arrow-up-short text-success position-absolute"></i> : <i className="bi bi-arrow-down-short text-danger position-absolute"></i>}
+                            {calculatePercentageChange(portfolio.last_equity, portfolio.equity) > 0 ? <i className="bi bi-arrow-up-short text-success"></i> : <i className="bi bi-arrow-down-short text-danger"></i>}
                         </span>
                     </div>
                 </>

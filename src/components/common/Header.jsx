2a0db60b4dc5117a7c2b3920ca182a0420daf318
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-20 16:42:27 +0000

    update navlinks

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from 'react-router-dom';
 
-const Header = (props) => {
+const Header = () => {
     return <nav className="navbar fixed-top shadow">
         <div className="container-fluid">
             <button className="btn border-0 shadow-0 position-fixed px-1 py-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" >
@@ -13,8 +13,8 @@ const Header = (props) => {
                     <div type="button" data-bs-dismiss="offcanvas" aria-label="Close" className="w-100 d-block mb-2"><i className="bi bi-x-lg float-end"></i></div>
                     <ul className="p-0 list-inline">
                         <li><NavLink to="/" className="text-decoration-none" role="button"><i className="bi bi-house-door d-block"></i>Home</NavLink></li>
-                        <li><NavLink to="/signup" className="text-decoration-none" role="button"><i className="bi bi-person-square d-block"></i>Sign Up</NavLink></li>
-                        <li><NavLink to="/portfolio" className="text-decoration-none" role="button"><i className="bi bi-graph-up d-block"></i>Porfolio</NavLink></li>
+                        <li><NavLink to="/login" className="text-decoration-none" role="button"><i className="bi bi-person-square d-block"></i>Login</NavLink></li>
+                        <li><NavLink to="/portfolio" className="text-decoration-none" role="button"><i className="bi bi-graph-up d-block"></i>Paper Trading</NavLink></li>
                         <li><NavLink to="/about" className="text-decoration-none" role="button"><i className="bi bi-info-square d-block"></i>About AIQ</NavLink></li>
                         <li><NavLink to="/contact" className="text-decoration-none" role="button"><i className="bi bi-envelope d-block"></i>Get in Touch</NavLink></li>
                     </ul>

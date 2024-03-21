22864bae0748d7342c5e4fe39f5a39432abe43fa
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-21 12:15:07 +0000

    fix npm run test error on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Login from './components/pages/Login';
 import About from './components/pages/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/pages/Contact';
-
+import { selectUsers } from './store/usersSlice.js';
+import { useSelector } from 'react-redux';
 
 
 const App = () => {
@@ -15,11 +16,9 @@ const App = () => {
       <Route path="/" element={<Container />}></Route>
       <Route path="signup" element={<SignUp />}></Route>
       <Route path="login" element={<Login />}></Route>
-      <Route path="portfolio" element={<Portfolio />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="login" element={<Login />} />
-
+      <Route path="portfolio" element={<Portfolio />}></Route>
+      <Route path="about" element={<About />}></Route>
+      <Route path="contact" element={<Contact />}></Route>
     </Routes>
   </>
 }

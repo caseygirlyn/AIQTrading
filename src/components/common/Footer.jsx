af9f0d5fac46649e27a2774db56ac58cc16afc30
Girlyn Antillon <casey.girlyn@gmail.com>
2024-05-31 10:11:05 +0100

    install google tag manager

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import GoogleTagManager from './GoogleTagManager'
 
 function Footer() {
     return (
-        <footer className="border-top py-3 w-100 mt-5 text-center">
-            <span className="text-muted">© 2024 AIQTrading</span>
-        </footer>
+        <>
+            <footer className="border-top py-3 w-100 mt-5 text-center">
+                <span className="text-muted">© 2024 AIQTrading</span>
+            </footer>
+            <GoogleTagManager />
+        </>
     )
 }
 

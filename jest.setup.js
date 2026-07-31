020fa0b3b5cf5b4d3497ac40ef4036f41a83c98e
Girlyn Antillon <casey.girlyn@gmail.com>
2026-07-31 16:40:44 +0100

    fix(deps): upgrade React Router and add Jest TextEncoder polyfill

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,22 @@
 import '@testing-library/jest-dom';
 import ResizeObserver from 'resize-observer-polyfill';
 import 'jest-canvas-mock';
+import { TextDecoder, TextEncoder } from 'util';
 
 global.ResizeObserver = ResizeObserver;
+global.TextDecoder = TextDecoder;
+global.TextEncoder = TextEncoder;
   
 console.log('Jest setup file executed');
+
+// Suppress React Router v6 deprecation warnings
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (args[0]?.includes?.('React Router') && args[0]?.includes?.('Future Flag Warning')) {
+    return;
+  }
+  originalWarn(...args);
+};
 
 jest.mock('chart.js', () => {
     const originalModule = jest.requireActual('chart.js');
